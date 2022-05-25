289ee1f2097b2c939c6838cb3e451a1a9cce91f3
shubhamdhama10 <shubham.dhama@srijan.net>
2022-05-25 21:14:44 +0530

    created order model

--- src/main/java/com/shubham/project/entity/Product.java
+++ src/main/java/com/shubham/project/entity/Product.java
@@ -1,8 +1,14 @@
 package com.shubham.project.entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -17,13 +23,22 @@ import lombok.NoArgsConstructor;
 public class Product {
 	
 	@Id
-	private Integer id;
+	@GeneratedValue
+	private int id;
+	
 	@Column(name = "ProductName")
 	private String productName;
+	
 	@Column(name = "PartNumber")
-	private Integer partNo;
+	private int partNo;
+	
 	@Column(name = "ProductLabel")
 	private String productLabel;
+	
+//	@OneToMany(mappedBy = "product")
+	@OneToMany(targetEntity = Order.class,cascade = CascadeType.ALL)
+	@JoinColumn(name = "po_fk",referencedColumnName = "id")
+	private List<Order> orders;
 	
 
 }
