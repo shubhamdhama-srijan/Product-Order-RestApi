7cccc2a5ff61844189e70ed876fba41c3701a09c
Bhawana Gaur <bhawana.gaur@srijan.net>
2022-05-26 09:43:54 +0530

    Added order service file

---
+++ src/main/java/com/shubham/project/service/OrderService.java
@@ -0,0 +1,26 @@
+package com.shubham.project.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shubham.project.entity.Order;
+import com.shubham.project.repository.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository orderRepository;
+
+	public List<Order> getAllOrders() {
+		return orderRepository.findAll();
+	}
+
+	public Order createOrder(Order order) {
+		return orderRepository.save(order);
+	}
+	
+	
+}
