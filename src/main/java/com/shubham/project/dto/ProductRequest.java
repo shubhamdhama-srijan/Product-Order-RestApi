cebadbf67c6840dd199bf2de44077931d92ffacb
Bhawana Gaur <bhawana.gaur@srijan.net>
2022-05-27 17:08:41 +0530

    added validation

---
+++ src/main/java/com/shubham/project/dto/ProductRequest.java
@@ -0,0 +1,36 @@
+package com.shubham.project.dto;
+
+import java.util.List;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import com.shubham.project.entity.Order;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor(staticName = "build")
+@NoArgsConstructor
+public class ProductRequest {
+	
+	@NotBlank(message = "Product Name should not be Blank!!")
+	@NotNull(message = "Product Name should not be null!!")
+	private String productName;
+	
+	@Pattern(regexp = "^\\d{6}$",message = "Invalid Part number number entered!!")
+	private String partNo;
+	
+	@NotBlank(message = "Product Label should not be Blank!!")
+	@NotNull(message = "Product Label should not be null!!")
+	private String productLabel;
+	
+	private List<Order> orders;
+	
+//	private List<Purchase> purchases;
+
+
+}
