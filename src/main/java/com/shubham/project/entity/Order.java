d41cbd05481bf67549a940c5eec94acde4698ae0
shubhamdhama10 <shubham.dhama@srijan.net>
2022-05-27 17:29:54 +0530

    modified product list inside order model

--- src/main/java/com/shubham/project/entity/Order.java
+++ src/main/java/com/shubham/project/entity/Order.java
@@ -8,6 +8,9 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,6 +27,7 @@ public class Order {
 	private int ordId;
 	
 	@ManyToOne
+	@JsonProperty(access = Access.WRITE_ONLY)
 	private Product product;
 	
 //	@Column(name = "OrderTitle")
