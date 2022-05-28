a464bc78f5cb809bf87d87165aea1a93e699997c
Bhawana Gaur <bhawana.gaur@srijan.net>
2022-05-28 18:52:23 +0530

    Implemented purchase entity

--- src/main/java/com/shubham/project/entity/Purchase.java
+++ src/main/java/com/shubham/project/entity/Purchase.java
@@ -1,5 +1,39 @@
 package com.shubham.project.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Purchase_table")
+@Data
+@AllArgsConstructor(staticName = "build")
+@NoArgsConstructor
 public class Purchase {
 
+	@Id
+	@GeneratedValue
+	private int purchId;
+	
+	@Column(name = "purchaseDate")
+	private Date purchDate;
+	
+	@ManyToOne
+	@JsonProperty(access = Access.WRITE_ONLY)
+	private Product product;
+	
+	
+	
 }
